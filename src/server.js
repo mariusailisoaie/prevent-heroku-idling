48ad40f001e50f90b003358f964cf78c2e5f7001
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2021-01-09 16:05:53 +0100

    Use setInterval to ping app instead of node-cron

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import express, { Router } from 'express'
 import serverless from 'serverless-http'
-import { schedule } from 'node-cron'
 import axios from 'axios'
 
 const app = express()
@@ -8,13 +7,14 @@ const router = Router()
 
 process.env.SILENCE_EMPTY_LAMBDA_WARNING = true
 
-schedule('* * * * * *', async () => {
-  const { data } = await axios.get('https://crown-clothing-copenhagen.herokuapp.com/api/ping')
-  console.log(data)
-})
-
 router.get('/', async (req, res) => {
   res.send('hello')
+
+  setInterval(async () => {
+    const { data } = await axios.get('https://crown-clothing-copenhagen.herokuapp.com/api/ping')
+    console.log(data)
+
+  }, 1000)
 })
 
 app.use('/.netlify/functions/server', router)
